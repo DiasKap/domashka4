7e2ba6bd64754021e1c9b8b3cd9ab4d5d8cd6e7b
DiasKapassov <diasgoose@gmail.com>
2023-04-05 14:55:46 +0600

    Выполнил 2 задание

---
+++ zadanie2/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = int.Parse(Console.ReadLine());
+int sum = 0;
+Console.Write($"Сумма чисел {num} равна {SUMMA(num)}");
+
+int SUMMA(int num)
+{
+    int num_a = num;
+    int sum = 0;
+    while (num_a > 0)
+    {
+        sum += num_a % 10;
+        num_a /= 10;
+    }
+    return sum;
+}
