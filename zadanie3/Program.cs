8d93c580b212b0742b734425c25f68db124cb4f2
DiasKapassov <diasgoose@gmail.com>
2023-04-05 14:56:26 +0600

    Закончил 3 задание

---
+++ zadanie3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int a = 0; a < size; a++)
+    {
+        result[a] = new Random().Next(100);
+    }
+    return result;
+}
+
+int size = 18;
+Console.WriteLine($"[{String.Join(" | ", Array(size))}]");
