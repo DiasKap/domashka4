9826ec56b56f8f779a4212bb5df90495bee24c4d
DiasKapassov <diasgoose@gmail.com>
2023-04-05 14:55:19 +0600

    Сделал 1 задание

---
+++ zadanie1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Stepen(int Anumber, int Bnumber){
+  int result = 1;
+  for(int i=1; i <= Bnumber; i++){
+    result = result * Anumber;
+  }
+    return result;
+}
+  Console.Write("Число A: ");
+  int Anumber = int.Parse(Console.ReadLine());
+  Console.Write("Число B: ");
+  int Bnumber = int.Parse(Console.ReadLine());
+  int stepen = Stepen(Anumber, Bnumber);
+  Console.WriteLine("Равно: " + stepen);
